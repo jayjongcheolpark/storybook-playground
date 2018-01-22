ff85b0daef3d8b410da223eac54b7622026994f4
Jay Park <jay.jongcheol.park@gmail.com>
2018-01-21 20:46:06 -0500

    Storybook basic

---
+++ src/components/Input/Input.stories.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { storiesOf, addDecorator } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import backgrounds from '@storybook/addon-backgrounds'
+import Input from './Input'
+
+storiesOf('Input', module)
+  .addDecorator((story) => <div style={{ margin: '50px'}} >{story()}</div>)
+  .addDecorator(backgrounds([
+    { name: "white", value: "#ffffff", default: true },
+    { name: "twitter", value: "#00aced" },
+    { name: "facebook", value: "#3b5998" },
+  ]))
+  .add('default', () => <Input onChange={action('changed')} />)
+  .add('disabled', () => <Input disabled />)
